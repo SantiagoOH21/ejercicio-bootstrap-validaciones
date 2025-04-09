fe8862abc0bdc9c1a3d7d3e655fbc4842d00ee1a
SantiagoOH21 <soh0621@gmail.com>
2025-04-09 15:23:31 +0200

    image

--- main.js
+++ main.js
@@ -114,9 +114,9 @@ if (document.getElementById("userForm")) {
   function addUser() {
     const usersLocalStorage =
       JSON.parse(localStorage.getItem("usersLocalStorage")) || [];
+
     const userName = inputName.value.trim();
     const userEmail = inputEmail.value.trim();
-
     const registerFind = usersLocalStorage.find(
       (user) => user.email === userEmail
     );
@@ -125,9 +125,12 @@ if (document.getElementById("userForm")) {
       return;
     }
 
+    const sexValue = getUserSex();
+
     const newUser = {
       name: userName,
       email: userEmail,
+      sex: sexValue,
     };
 
     usersLocalStorage.push(newUser);
@@ -138,6 +141,21 @@ if (document.getElementById("userForm")) {
     userForm.reset();
     showMessage("success", "Usuario creado correctamente");
     redirectToUsers();
+  }
+
+  function getUserSex() {
+    const userSex = document.getElementsByName("userSex");
+    let sexValue = null;
+
+    for (let i = 0; i < userSex.length; i++) {
+      if (userSex[i].checked) {
+        sexValue = userSex[i].value;
+        break;
+      }
+    }
+
+    console.log("Valor seleccionado:", sexValue);
+    return sexValue;
   }
 
   function redirectToUsers() {
@@ -177,11 +195,22 @@ if (document.getElementById("userForm")) {
     div.className = "card text-bg-light";
     div.style.width = "18rem";
 
+    //img
     const image = document.createElement("img");
-    image.src =
-      "https://cdn.pixabay.com/photo/2023/08/05/15/42/panda-8171354_640.jpg";
-    // "https://cdn.pixabay.com/photo/2022/03/09/04/57/zebra-7057023_640.jpg";
-    // "https://cdn.pixabay.com/photo/2023/12/07/19/45/tiger-8436227_640.jpg";
+
+    if (user.sex === "woman") {
+      image.src =
+        "https://cdn.pixabay.com/photo/2017/10/20/22/28/cougar-2873099_640.jpg";
+    } else if (user.sex === "man") {
+      image.src =
+        "https://cdn.pixabay.com/photo/2018/01/08/15/23/leopard-3069562_640.jpg";
+    } else if (user.sex === "nonBinary") {
+      image.src =
+        "https://cdn.pixabay.com/photo/2023/12/07/19/45/tiger-8436227_640.jpg";
+    } else {
+      image.src =
+        "https://cdn.pixabay.com/photo/2023/08/05/15/42/panda-8171354_640.jpg";
+    }
 
     image.className = "card-img-top";
     image.alt = "Imagen de usuario";
@@ -197,8 +226,8 @@ if (document.getElementById("userForm")) {
     paragraph.className = "card-text";
     paragraph.textContent = `${user.email}`;
 
-    div2.appendChild(paragraph);
     div2.appendChild(titleCard);
+    div2.appendChild(paragraph);
     div.appendChild(image);
     div.appendChild(div2);
     usersContainer.appendChild(div);
